a88b4deac50a2b243e639ec92de09804b402ff32
Manoj PN <manojpnarayann@gmail.com>
2026-05-23 12:04:56 +0530

    mapper any removed

--- backend/src/infrastructure/database/Mappers/TransactionPersistanceMapper.ts
+++ backend/src/infrastructure/database/Mappers/TransactionPersistanceMapper.ts
@@ -2,22 +2,42 @@ import { Transactions } from "../../../domain/entities/Transaction.entity";
 import { ITransactionDocument } from "../models/TransactionModel";
 import { TransactionPurpose } from "../../../domain/entities/Transaction.entity";
 
+interface PopulatedUser{
+    _id:{toString():string};
+    name:string;
+}
+
+interface PopulatedAuction{
+    _id:{toString():string};
+    title:string;
+}
+
+function isPopulatedUser(obj:unknown):obj is PopulatedUser{
+    return !!obj && typeof obj === 'object' && 'name' in obj;
+}
+
+function isPopulatedAuction(obj:unknown):obj is PopulatedAuction{
+    return !!obj && typeof obj === 'object' && 'title' in obj
+}
 
 export class TransactionPersistanceMapper{
     static toEntity(doc:ITransactionDocument):Transactions{
-        const user = doc.userId as unknown as { _id: any, name?: string };
-        const auction = doc.auctionId as unknown as { _id: any, title?: string };
-        const userName=user.name;
-        const auctionTitle=auction? auction.title : undefined
+        const user = doc.userId
+        const userName= isPopulatedUser(user) ? user.name : undefined;
+        const userIdStr=isPopulatedUser(user) ? user._id.toString() : user.toString();
+
+        const auction = doc.auctionId
+        const auctionTitle = isPopulatedAuction(auction)? auction.title : undefined;
+        const auctionIdStr=auction ? (isPopulatedAuction(auction) ? auction._id.toString() : auction.toString() ) : undefined;
         return new Transactions(
             doc._id.toString(),
-            user && user._id ? user._id.toString() : doc.userId.toString(),
+            userIdStr,
             doc.walletId.toString(),
             doc.amount,
             doc.type,
             doc.status,
             doc.purpose as TransactionPurpose,
-            doc.auctionId?.toString(),
+            auctionIdStr,
             doc.stripePaymentIntentId,
             doc.description,
             doc.isReleased,
